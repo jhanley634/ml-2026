f29328fc1a3fa92ddb4d4b36b6ad53fe315b48b5
John Hanley <jhanley634@users.noreply.github.com>
2025-08-01 15:46:37 -0700

    Add new module

--- camera/mirror.py
+++ camera/mirror.py
@@ -1,11 +1,8 @@
 #! /usr/bin/env python
 
 import cv2
-import cv2.data
 
-
-def _key() -> str:
-    return chr(cv2.waitKey(1) & 0xFF).upper()
+from camera.face_finder import _key, face_finder
 
 
 def mirror() -> None:
@@ -23,37 +20,6 @@ def mirror() -> None:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         cv2.imshow("Mirror", gray)
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-VIOLET = (255, 0, 255)  # BGR
-
-
-def face_finder() -> None:
-    """
-    Draws a violet bounding box around the single face in the captured camera image.
-    """
-
-    cap = cv2.VideoCapture(0)
-    assert cap
-
-    face_cascade = cv2.CascadeClassifier(
-        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-    )
-
-    while _key() != "Q":
-        ret, frame = cap.read()
-        assert ret
-        frame = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-        for x, y, w, h in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), VIOLET, 4)
-
-        cv2.imshow("Face Finder", frame)
 
     cap.release()
     cv2.destroyAllWindows()
