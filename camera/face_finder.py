df1759b80e58307311e4e37c329de0f87bd04b4b
John Hanley <jhanley634@users.noreply.github.com>
2025-08-01 16:13:31 -0700

    Add _order_by_size(), and show at most 2 faces

--- camera/face_finder.py
+++ camera/face_finder.py
@@ -1,10 +1,22 @@
 import cv2
 import cv2.data
+import numpy as np
+from numpy._typing import NDArray
 
 
 def _key() -> str:
     return chr(cv2.waitKey(1) & 0xFF).upper()
 
+
+def _order_by_size(rectangle: NDArray[np.int32]) -> int:
+    r = rectangle
+    assert r.shape == (4,)  # x, y, w, h
+    assert r.dtype == np.dtype(np.int32)
+    assert r[2] == r[3]
+    return int(r[3])
+
+
+MAX_FACES = 2
 
 VIOLET = (255, 0, 255)  # BGR
 
@@ -26,9 +38,10 @@ def face_finder() -> None:
         assert ret
         frame = cv2.flip(frame, 1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        faces = np.array(face_cascade.detectMultiScale(gray, 1.1, 4))
+        faces2 = sorted(faces, reverse=True, key=_order_by_size)[:MAX_FACES]
 
-        for x, y, w, h in faces:
+        for x, y, w, h in faces2:
             cv2.rectangle(frame, (x, y), (x + w, y + h), VIOLET, 4)
 
         cv2.imshow("Face Finder", frame)
