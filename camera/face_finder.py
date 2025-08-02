c85625c83432f1f8a6575a56ac8dbb8d43c37d91
John Hanley <jhanley634@users.noreply.github.com>
2025-08-01 17:38:01 -0700

    Display each frame, even if no face was found

--- camera/face_finder.py
+++ camera/face_finder.py
@@ -47,23 +47,22 @@ def face_finder() -> None:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = np.array(face_cascade.detectMultiScale(gray, 1.1, 4))
         faces2 = sorted(faces, reverse=True, key=order_by_size)[:MAX_FACES]
-        if len(faces2) == 0:
-            continue
+        if len(faces2) > 0:
 
-        x2, y2, w2, h2 = current_rect = faces2[0]
+            x2, y2, w2, h2 = current_rect = faces2[0]
 
-        if prev_rect is not None:
-            x1, y1, w1, h1 = prev_rect
+            if prev_rect is not None:
+                x1, y1, w1, h1 = prev_rect
 
-            # Smooth the movement of the rectangle
-            x = int(x1 + SMOOTH_FRAC * (x2 - x1))
-            y = int(y1 + SMOOTH_FRAC * (y2 - y1))
-            w = int(w1 + SMOOTH_FRAC * (w2 - w1))
-            h = int(h1 + SMOOTH_FRAC * (h2 - h1))
+                # Smooth the movement of the rectangle
+                x = int(x1 + SMOOTH_FRAC * (x2 - x1))
+                y = int(y1 + SMOOTH_FRAC * (y2 - y1))
+                w = int(w1 + SMOOTH_FRAC * (w2 - w1))
+                h = int(h1 + SMOOTH_FRAC * (h2 - h1))
 
-            cv2.rectangle(frame, (x, y), (x + w, y + h), VIOLET, 4)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), VIOLET, 4)
 
-        prev_rect = current_rect
+            prev_rect = current_rect
 
         frame_count += 1
         if frame_count % 30 == 0:
