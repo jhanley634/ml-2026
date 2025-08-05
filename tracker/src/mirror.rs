6f53f6676fbcaf8ecde10cf8806bfbeecb22d42f
John Hanley <jhanley634@users.noreply.github.com>
2025-08-05 15:11:27 -0700

    Move business logic into src/mirror.rs

---
+++ tracker/src/mirror.rs
@@ -0,0 +1,46 @@
+use opencv::{
+    core::{AlgorithmHint, Mat, flip},
+    highgui::{destroy_all_windows, imshow, wait_key},
+    imgproc::{ColorConversionCodes, cvt_color},
+    prelude::*,
+    videoio::VideoCapture,
+};
+
+pub fn mirror() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hit 'q' to quit");
+
+    let mut cap = VideoCapture::new(0, 0)?;
+
+    loop {
+        let mut frame = Mat::default();
+        let ret = cap.read(&mut frame)?;
+        if !ret {
+            break;
+        }
+
+        // Convert to grayscale
+        let mut gray = Mat::default();
+        cvt_color(
+            &frame,
+            &mut gray,
+            ColorConversionCodes::COLOR_BGR2GRAY as i32,
+            0,
+            AlgorithmHint::ALGO_HINT_DEFAULT,
+        )?;
+
+        let mut flipped_frame = Mat::default();
+        flip(&gray, &mut flipped_frame, 1)?; // horizontally
+
+        imshow("Mirror", &flipped_frame)?;
+
+        let key = wait_key(1)?;
+        if key == 'q' as i32 {
+            break;
+        }
+    }
+
+    cap.release()?;
+    destroy_all_windows()?;
+
+    Ok(())
+}
