57abfad5551e2e39f499f5ef91b43ad24418d96c
John Hanley <jhanley634@users.noreply.github.com>
2025-08-07 16:34:35 -0700

    Add EDA

---
+++ solubility/eda.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+# from:
+# https://www.kaggle.com/code/kerneler/starter-aqsoldb-a-curated-aqueous-6146b12d-1
+
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
