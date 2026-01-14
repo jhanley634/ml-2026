7f8912118dd4ce3f8d6d42a9781638b97c8bb02a
John Hanley <jhanley634@users.noreply.github.com>
2026-01-14 11:30:59 -0800

    Use hypothesis

--- so_2026/m01/two_way_merge_test.py
+++ so_2026/m01/two_way_merge_test.py
@@ -1,10 +1,13 @@
 import unittest
 
+from hypothesis import given
+from hypothesis import strategies as st
+
 from so_2026.m01.two_way_merge import merge
 
 
 class TwoWayMergeTest(unittest.TestCase):
-    def test_find_median(self) -> None:
+    def test_merge(self) -> None:
         a = list(range(2))
         b = list(range(4))
         self.assertEqual(
@@ -14,4 +17,19 @@ class TwoWayMergeTest(unittest.TestCase):
         self.assertEqual(
             [0, 0, 1, 1, 2, 3],
             list(merge(b, a)),
+        )
+
+    @given(st.lists(st.floats(), min_size=1), st.lists(st.floats(), min_size=1))
+    def test_hypothesis_merge(self, a: list[float], b: list[float]) -> None:
+        a.sort()
+        b.sort()
+        merged_list = list(merge(a, b))
+
+        self.assertEqual(merged_list, sorted(merged_list))  # monotonic
+        self.assertEqual(merged_list, sorted(a + b))
+        self.assertTrue(all(item in merged_list for item in a))
+        self.assertTrue(all(item in merged_list for item in b))
+        self.assertEqual(
+            len(merged_list),
+            len(a) + len(b),
         )
