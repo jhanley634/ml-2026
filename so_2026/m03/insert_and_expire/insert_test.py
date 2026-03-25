f7c0fc5113f9e3c7eb83f1982527ada01d3004b7
John Hanley <jhanley634@users.noreply.github.com>
2026-03-24 17:58:53 -0700

    Verify that the expected number of rows were in fact INSERTed

--- so_2026/m03/insert_and_expire/insert_test.py
+++ so_2026/m03/insert_and_expire/insert_test.py
@@ -1,7 +1,7 @@
 import unittest
 from time import time
 
-from so_2026.m03.insert_and_expire.insert import create_empty_table, ins
+from so_2026.m03.insert_and_expire.insert import ReqLog, create_empty_table, get_session, ins
 
 
 class InsertTest(unittest.TestCase):
@@ -10,10 +10,15 @@ class InsertTest(unittest.TestCase):
         ins(1)
 
         elapsed_limit = 0.1
+        num_recs = 4_000
         t0 = time()
-        ins(4_000)  # at least 40k (often ~ 50k) rows per second
+        ins(num_recs)  # at least 40k (often ~ 50k) rows per second
         elapsed = time() - t0
 
         if verbose:
             print(f"\n{elapsed=:06f}")
         self.assertLess(elapsed, elapsed_limit)
+
+        with get_session() as sess:
+            result = sess.query(ReqLog).all()
+            self.assertEqual(num_recs + 1, len(result))
