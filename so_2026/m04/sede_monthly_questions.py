5538e38173d8f3a4bff3bbf36f34a8b6a38c4061
John Hanley <jhanley634@users.noreply.github.com>
2026-04-27 10:12:29 -0700

    Add a stair-step 'september' column

--- so_2026/m04/sede_monthly_questions.py
+++ so_2026/m04/sede_monthly_questions.py
@@ -3,8 +3,6 @@
 # data from:
 # https://data.stackexchange.com/stackoverflow/query/1882532/questions-per-month
 
-# chart posted on:
-# https://stackoverflow.com/questions/72695327/sql-display-max-2-employees-per-dept
 
 from pathlib import Path
 
@@ -27,25 +25,43 @@ def plot(
             [
                 pl.col("Created").str.slice(0, 10).cast(pl.Date).alias("month"),
                 pl.col("").alias("questions"),
-            ]
+            ],
         )
         .select(["month", "questions"])
     )
     df = df.with_columns(pl.col("questions").rolling_mean(window_size=12).alias("EMA_questions"))
+
+    # df = df.with_columns(pl.lit(0).alias("september"))
+
+    df = df.with_columns(
+        pl.when(pl.col("month").dt.month() == 9)
+        .then(pl.col("month").dt.year().cast(pl.Int64))
+        .alias("september_year"),
+    )
+    # Create a 12-month stair step effect by forward filling the September values.
+    df = df.with_columns(
+        pl.when(pl.col("september_year") == pl.col("month").dt.year())
+        .then(pl.col("questions"))
+        .otherwise(None)
+        .alias("september"),
+    ).with_columns(
+        pl.col("september").fill_null(pl.col("september").rolling_max(window_size=12).shift(1)),
+    )
     print(df)
 
     sns.set(style="whitegrid")
     plt.figure(figsize=(12, 6))
     ax = sns.lineplot(data=df.to_pandas(), x="month", y="questions", marker="o")
     sns.lineplot(
+        ax=ax,
         data=df.to_pandas(),
         x="month",
         y="EMA_questions",
         linestyle="--",
         color="purple",
-        ax=ax,
         label="moving avg.",
     )
+    # sns.lineplot(ax=ax, data=df.to_pandas(), x="month", y="september", color="red", label="Sept")
     ax.set_xlabel("")
     ax.set_ylabel("Number of Questions")
     ax.xaxis.set_major_locator(mdates.YearLocator())
