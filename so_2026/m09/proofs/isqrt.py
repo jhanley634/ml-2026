80badd646a4c83876b85f9d312a990abfa24029f
John Hanley <jhanley634@users.noreply.github.com>
2026-09-08 15:00:22 -0700

    Add hypothesis _test_isqrt()

--- so_2026/m09/proofs/isqrt.py
+++ so_2026/m09/proofs/isqrt.py
@@ -2,6 +2,11 @@
 
 from math import sqrt
 
+import hypothesis.strategies as st
+from hypothesis import given
+
+TOO_BIG = 72_057_594_037_927_932
+
 
 def isqrt(n: int) -> int:
     """
@@ -11,7 +16,6 @@ def isqrt(n: int) -> int:
         raise ValueError
 
     r = n
-
     while r > 0 and abs(r - (n // r)) > 1:
         r = (r + n // r) // 2
 
@@ -19,10 +23,21 @@ def isqrt(n: int) -> int:
         r -= 1
 
     assert r**2 <= n < (r + 1) ** 2
-    assert r == int(sqrt(n))
+    if n < TOO_BIG:
+        assert r == int(sqrt(n)), (sqrt(n), r, n)
     return r
 
 
+@given(st.integers(min_value=0))
+def _test_isqrt(n):
+    r = isqrt(n)
+    assert r**2 <= n < (r + 1) ** 2
+
+
 if __name__ == "__main__":
+    isqrt(TOO_BIG - 1)  # This is 0x1.fffffffffffffp+55
+    isqrt(TOO_BIG)
+    _test_isqrt()
+
     for i in range(30):
-        print(i, isqrt(i))
+        isqrt(i)
